d8c3637c3060b5219923b6ecd54b4e98c545539a
Raphaël Grougnet <raphaelgrougnet83000@gmail.com>
2023-10-05 19:24:02 -0400

    Ajout de commentaires pour PaquetController

--- controllers/paquetController.js
+++ controllers/paquetController.js
@@ -1,3 +1,4 @@
+// Importation des modèles nécessaires
 const Paquet = require('../models/paquet');
 const User = require('../models/user');
 const Carte = require('../models/carte');
@@ -6,14 +7,17 @@ const Carte = require('../models/carte');
 // Récupérer tous les paquets
 exports.getPaquets = async (req, res, next) => {
   try {
+    // Récupération de tous les paquets
     const paquets = await Paquet.find();
     if (!paquets) {
       const error = new Error('Aucun paquet trouvé !');
       error.statusCode = 404;
       throw error;
     }
+    // Envoi des paquets en réponse
     res.status(200).json(paquets);
   } catch (err) {
+    // Gestion des erreurs
     if (!err.statusCode) {
       err.statusCode = 500;
     }
@@ -23,8 +27,10 @@ exports.getPaquets = async (req, res, next) => {
 
 // Créer un nouveau paquet
 exports.createPaquet = async (req, res, next) => {
+  // Récupération des données du paquet à partir de la requête
   const { nom, description, userId } = req.body;
 
+  // Création d'un nouvel objet Paquet
   const paquet = new Paquet({
     nom,
     description,
@@ -32,32 +38,41 @@ exports.createPaquet = async (req, res, next) => {
   });
 
   try {
+    // Vérification de l'existence de l'utilisateur
     if (!userId) {
       const error = new Error('Le paquet doit être associé à un utilisateur (un userId est requis)');
       error.statusCode = 400;
       throw error;
     }
 
+    // Recherche de l'utilisateur par son ID
     const user = await User.findById(userId);
 
+    // Si l'utilisateur n'existe pas, on lance une erreur
     if (!user) {
       const error = new Error('Aucun utilisateur trouvé !');
       error.statusCode = 404;
       throw error;
     }
 
+    // Vérification de l'existence d'un paquet avec le même nom pour le même utilisateur
     const paquetExiste = await Paquet.findOne({ nom, userId });
 
+    // Si un tel paquet existe, on lance une erreur
     if (paquetExiste) {
       const error = new Error('Un paquet avec ce nom existe déjà !');
       error.statusCode = 400;
       throw error;
     }
 
+    // Sauvegarde du nouveau paquet dans la base de données
     const paquetCree = await paquet.save();
+    // Ajout de l'URL du nouveau paquet dans l'en-tête Location de la réponse
     res.location("/paquet/" + paquetCree.id);
+    // Envoi du paquet créé en réponse
     res.status(201).json(paquetCree);
   } catch (error) {
+    // Gestion des erreurs
     if (error.name === 'ValidationError') {
       error.statusCode = 400;
     }
@@ -70,27 +85,33 @@ exports.createPaquet = async (req, res, next) => {
 
 // Récupérer un paquet spécifique
 exports.getPaquet = async (req, res, next) => {
+  // Récupération de l'ID du paquet à partir de la requête
   const id = req.params.id;
 
   try {
+    // Vérification de la validité de l'ID du paquet
     if (id.length !== 12 && id.length !== 24) {
       const error = new Error('ID de paquet invalide');
       error.statusCode = 400;
       throw error;
     }
 
+    // Recherche du paquet par son ID
     const paquet = await Paquet.findById(id);
     //! const cartes = await Carte.find({ paquetId: id });
     //! paquet.cartes = cartes;
 
+    // Si le paquet n'existe pas, on lance une erreur
     if (!paquet) {
       const error = new Error('Aucun paquet trouvé !');
       error.statusCode = 404;
       throw error;
     }
 
+    // Envoi du paquet en réponse
     res.status(200).json(paquet);
   } catch (error) {
+    // Gestion des erreurs
     if (error.name === 'ValidationError') {
       error.statusCode = 400;
     }
@@ -101,32 +122,41 @@ exports.getPaquet = async (req, res, next) => {
   }
 };
 
+// Mettre à jour un paquet spécifique
 exports.updatePaquet = async (req, res, next) => {
+  // Récupération de l'ID du paquet et des données à mettre à jour à partir de la requête
   const id = req.params.id;
   const { nom, description, userId } = req.body;
 
   try {
+    // Vérification de la validité de l'ID du paquet
     if (id.length !== 12 && id.length !== 24) {
       const error = new Error('ID de paquet invalide');
       error.statusCode = 400;
       throw error;
     }
 
+    // Recherche du paquet par son ID
     const paquet = await Paquet.findById(id);
 
+    // Si le paquet n'existe pas, on lance une erreur
     if (!paquet) {
       const error = new Error('Aucun paquet trouvé !');
       error.statusCode = 404;
       throw error;
     }
 
+    // Mise à jour des données du paquet
     paquet.nom = nom;
     paquet.description = description;
     paquet.userId = userId;
 
+    // Sauvegarde du paquet modifié dans la base de données
     const paquetModifie = await paquet.save();
+    // Envoi du paquet modifié en réponse
     res.status(200).json(paquetModifie);
   } catch (error) {
+    // Gestion des erreurs
     if (error.name === 'ValidationError') {
       error.statusCode = 400;
     }
@@ -139,34 +169,41 @@ exports.updatePaquet = async (req, res, next) => {
 
 // Supprimer un paquet spécifique
 exports.deletePaquet = async (req, res, next) => {
+  // Récupération de l'ID du paquet à partir de la requête
   const id = req.params.id;
 
   try {
+    // Vérification de la validité de l'ID du paquet
     if (id.length !== 12 && id.length !== 24) {
       const error = new Error('ID de paquet invalide');
       error.statusCode = 400;
       throw error;
     }
 
+    // Recherche du paquet par son ID
     const paquet = await Paquet.findById(id);
 
+    // Si le paquet n'existe pas, on lance une erreur
     if (!paquet) {
       const error = new Error('Aucun paquet trouvé !');
       error.statusCode = 404;
       throw error;
     }
 
+    // Suppression du paquet
     const promise = await paquet.remove();
 
+    // Si la suppression a échoué, on lance une erreur
     if (!promise) {
       const error = new Error('Erreur lors de la suppression du paquet !');
       error.statusCode = 500;
       throw error;
     }
 
-
+    // Envoi du paquet supprimé en réponse
     res.status(200).json(paquet);
   } catch (error) {
+    // Gestion des erreurs
     if (error.name === 'ValidationError') {
       error.statusCode = 400;
     }
@@ -179,19 +216,24 @@ exports.deletePaquet = async (req, res, next) => {
 
 // Rechercher un paquet par mot
 exports.recherchePaquets = async (req, res, next) => {
+  // Récupération du mot de recherche à partir de la requête
   const mot = req.params.mot;
 
   try {
+    // Recherche des paquets dont le nom contient le mot de recherche
     const paquets = await Paquet.find({ nom: { $regex: mot, $options: 'i' } }).sort({ createdAt: 1 });
 
+    // Si aucun paquet n'a été trouvé, on lance une erreur
     if (paquets.length === 0) {
       const error = new Error('Aucun paquet trouvé !');
       error.statusCode = 404;
       throw error;
     }
 
+    // Envoi des paquets trouvés en réponse
     res.status(200).json(paquets);
   } catch (error) {
+    // Gestion des erreurs
     if (error.name === 'ValidationError') {
       error.statusCode = 400;
     }
